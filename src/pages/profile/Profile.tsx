f4f364bcecbe0be6671078c58daf58813a30010f
khandeveloper <contact@iamkhan.net>
2023-11-17 01:21:32 -0600

    transition fix

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -273,7 +273,11 @@ const Profile = () => {
 			{matches ? (
 				<ProfileMobile />
 			) : (
-				<Box>
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					transition={{ duration: 1 }}
+					exit={{ opacity: '0', transition: { duration: 0.5 } }}>
 					<Parallax
 						ref={ref}
 						pages={4}
@@ -1039,7 +1043,7 @@ const Profile = () => {
 							</Fade>
 						</Box>
 					</Box>
-				</Box>
+				</motion.div>
 			)}
 		</Fragment>
 	);
